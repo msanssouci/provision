fd598aa54dc0bd898d952188d0b3ce3eaed10881
Mitchell Sans Souci <mitch.sans.souci@gmail.com>
2016-05-22 10:34:46 -0500

    Add script

---
+++ provision_laptop.sh
@@ -0,0 +1,12 @@
+sudo apt-get -y install thinkfan powertop
+sudo apt-get -y install tlp tlp-rdw acpi-call-dkms
+sudo apt-get -y install vagrant
+sudo apt-get -y install virtualbox
+sudo apt-get -y install vim
+sudo apt-get -y install git
+sudo apt-get -y install zsh
+sudo apt-get -y install gradle
+sudo apt-get -y install nodejs
+sudo apt-get -y install npm
+sudo apt-get -y install launchy
+#sudo apt-get -y install
